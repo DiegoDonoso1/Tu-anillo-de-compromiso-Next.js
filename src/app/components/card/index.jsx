2903a9e83786f20d2bd0894d2f70906b8217104a
Diego Donoso <81239084+DiegoDonoso1@users.noreply.github.com>
2023-07-02 19:09:41 -0400

    Agrego página anillos y detalle

--- src/app/components/card/index.jsx
+++ src/app/components/card/index.jsx
@@ -6,12 +6,11 @@ import style from './card.module.css';
 export default function Card() {
 	return (
 		<div className={style.card}>
-			<Link href="/detalle">
+			<Link href="/detail">
 				<Image src="/img/20230621_160101.jpg" alt="" width={260} height={260} />
 				<div className={style.mesage}>Comprar</div>
 			</Link>
-			<div className={style.card}>
-				<Link href="/detalle"></Link>
+			<div className={style.cart}>
 				<Link href="#">
 					<span className={style.cart_span}></span>
 				</Link>
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
-import styles from '../page.module.css';
 import Card from '../components/card';
+import style from './products.module.css';
+import Link from 'next/link';
 
 export default function page() {
 	return (
-		<main className={styles.main}>
-			<Card />
-		</main>
+		<>
+			<aside className={style.menu_filtros}>
+				<h3>Materiales</h3>
+				<ul>
+					<li>
+						<Link href="#">Plata</Link>
+					</li>
+					<li>
+						<Link href="#">Oro</Link>
+					</li>
+					<li>
+						<Link href="#">Plata y oro</Link>
+					</li>
+				</ul>
+				<h3>Rango de precio</h3>
+				<form action="#">
+					<input
+						type="range"
+						step={10000}
+						id="rango-precio"
+						min={0}
+						max={100000}
+					/>
+					<span id={style.texto_filtro} />
+					<button className={style.button_filtro}>Aplicar filtro</button>
+				</form>
+			</aside>
+
+			<section className={style.container_ordenar}>
+				<div className={style.cantidad}>
+					<p>7 productos</p>
+				</div>
+				<div className={style.filtro}>
+					<select name="orden" id={style.orden}>
+						<option value>Ordenar por</option>
+						<option value="saab">Precio: menor a mayor</option>
+						<option value="mercedes">Precio: mayor a menor</option>
+						<option value="audi">Nombre: a la z</option>
+					</select>
+				</div>
+			</section>
+
+			<main className={style.container_anillo}>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</main>
+
+			<section className={style.container_paginas}>
+				<div className={style.cambiar_pagina}>
+					<button className={style.left_button}>{'<'} Anterior </button>
+					<p>Página 1 de x</p>
+					<button className={style.right_button}>Siguiente {'>'}</button>
+				</div>
+			</section>
+		</>
 	);
 }
