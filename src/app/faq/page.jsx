ad54b1584059aaa54079b48410d12094d3c812ba
RatonConCola <132951682+RatonConCola@users.noreply.github.com>
2023-07-13 18:14:27 -0400

    Modificaciones a Preguntas Frecuentes, Texto añadido

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,78 +1,102 @@
 import React from 'react';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Faq() {
 	return (
 		<>
-			<main>
+			<main className={styles.pr_main}>
 				<div className={styles.pr_container}>
 					<div className={styles.pr_acord_container}>
 						<input className={styles.pr_input} type="checkbox" id="Titulo1" />
 						<label className={styles.pr_label} htmlFor="Titulo1">
-							Pregunta 1
+							¿De donde somos?
 						</label>
 						<div className={styles.Contenido_Acordeon}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-								quo ex, cupiditate molestias fugit eligendi dolor aspernatur
-								ullam impedit at, possimus eum iure aut similique ipsa non illum
-								ut? Dolore.
+								Somos de Freire, región de La Araucania, Chile.
 							</p>
 						</div>
 					</div>
 					<div className={styles.pr_acord_container}>
 						<input className={styles.pr_input} type="checkbox" id="Titulo2" />
 						<label className={styles.pr_label} htmlFor="Titulo2">
-							Pregunta 2
+							¿Hacen entregas presenciales?
 						</label>
 						<div className={styles.Contenido_Acordeon}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-								quo ex, cupiditate molestias fugit eligendi dolor aspernatur
-								ullam impedit at, possimus eum iure aut similique ipsa non illum
-								ut? Dolore.
+							Si, hacemos entregas presenciales en Freire, Pitrufquén y Temuco.
+
 							</p>
 						</div>
 					</div>
 					<div className={styles.pr_acord_container}>
 						<input className={styles.pr_input} type="checkbox" id="Titulo3" />
 						<label className={styles.pr_label} htmlFor="Titulo3">
-							Pregunta 3
+						¿Hacen envíos?
+
 						</label>
 						<div className={styles.Contenido_Acordeon}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-								quo ex, cupiditate molestias fugit eligendi dolor aspernatur
-								ullam impedit at, possimus eum iure aut similique ipsa non illum
-								ut? Dolore.
+								Si, a todo Chile, a través de Starken a domicilio o sucursal (por pagar) 
 							</p>
 						</div>
 					</div>
 					<div className={styles.pr_acord_container}>
 						<input className={styles.pr_input} type="checkbox" id="Titulo4" />
 						<label className={styles.pr_label} htmlFor="Titulo4">
-							Pregunta 4
+						 ¿Tienen tienda física?
 						</label>
 						<div className={styles.Contenido_Acordeon}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-								quo ex, cupiditate molestias fugit eligendi dolor aspernatur
-								ullam impedit at, possimus eum iure aut similique ipsa non illum
-								ut? Dolore.
+							No, por el momento somos un emprendimiento online.
 							</p>
 						</div>
 					</div>
 					<div className={styles.pr_acord_container}>
 						<input className={styles.pr_input} type="checkbox" id="Titulo5" />
 						<label className={styles.pr_label} htmlFor="Titulo5">
-							Pregunta 5
+						¿Qué materiales de joyas trabajan?
 						</label>
 						<div className={styles.Contenido_Acordeon}>
 							<p>
-								Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat
-								quo ex, cupiditate molestias fugit eligendi dolor aspernatur
-								ullam impedit at, possimus eum iure aut similique ipsa non illum
-								ut? Dolore.
+							Trabajamos Plata Ley 925, Plata con Oro de 14K.
+							</p>
+						</div>
+					</div>
+					<div className={styles.pr_acord_container}>
+						<input className={styles.pr_input} type="checkbox" id="Titulo6" />
+						<label className={styles.pr_label} htmlFor="Titulo6">
+						¿Tienen garantía los anillos?
+						</label>
+						<div className={styles.Contenido_Acordeon}>
+							<p>
+							Sólo por falla de fábrica, el cual será verificado por nuestros expertos si corresponde o no la garantía.
+							</p>
+						</div>
+					</div>
+					<div className={styles.pr_acord_container}>
+						<input className={styles.pr_input} type="checkbox" id="Titulo7" />
+						<label className={styles.pr_label} htmlFor="Titulo7">
+						¿Cómo sé mi talla de anillo?
+						</label>
+						<div className={styles.Contenido_Acordeon}>
+							<p>
+							Tenemos una guía disponible en la página, y un video explicativo. Para revisarlos haz </p>
+							<Link className={styles.pr_a} href="/sizeguide">
+											click aqui
+										</Link>
+							
+						</div>
+					</div>
+					<div className={styles.pr_acord_container}>
+						<input className={styles.pr_input} type="checkbox" id="Titulo8" />
+						<label className={styles.pr_label} htmlFor="Titulo8">
+						¿De qué otra forma puedo comunicarme con ustedes para resolver otras dudas? 
+						</label>
+						<div className={styles.Contenido_Acordeon}>
+							<p>
+							A través de nuestro WhatsApp: +569 79207723, estaremos felices de ayudarte.
 							</p>
 						</div>
 					</div>
